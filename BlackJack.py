d89ce9c13687f14d4c5247785d1ada471421b58b
Andrew <71112588+Hot-M3ss@users.noreply.github.com>
2024-09-12 22:49:16 -0400

    4th commit

--- BlackJack.py
+++ BlackJack.py
@@ -1,12 +1,15 @@
 import random
+import os
 
 current_game_deck = []
+discarded_game_deck = []
 players_hand = []
 dealers_hand = []
 suits = ['H','S','C','D']
 cards = ['A', 'J', 'Q', 'K']    
 running = True
 game_state = 'running'
+previous_round_message = ''
 player_cards_dealt = 0
 dealer_cards_dealt = 0
 stand_or_hit = ''
@@ -18,23 +21,24 @@ def run_game():
         if game_state == 'running':
             initial_deal()
         elif game_state == 'blackjack!':
-            running = False
             clear_hands()
         elif game_state == 'busted':
-            running = False
             clear_hands()
 
 def clear_hands():
     global players_hand, dealers_hand, player_cards_dealt, dealer_cards_dealt, running
+    
     # Clears all cards from the players hands
-    for x in players_hand:
-        players_hand.remove(x)
-    player_cards_dealt = 0
+    if player_cards_dealt > 0:
+        players_hand.clear()
+        player_cards_dealt = 0
+
     # Clears all cards from the dealers hand
-    for x in dealers_hand:
-        dealers_hand.remove(x)
-    dealer_cards_dealt = 0
-    running = False
+    if dealer_cards_dealt > 0:
+        dealers_hand.clear()
+        dealer_cards_dealt = 0
+    initial_deal()
+    game_state == 'running'
 
 def available_deck():
     global suits, cards
@@ -56,45 +60,49 @@ def initial_deal():
             if card in current_game_deck:
                 current_game_deck.remove(card)
         dealer_cards_dealt += 1
-        print(dealers_hand)
 
     # deal the initial cards to the player
-    if player_cards_dealt >= 2:
-        deal_cards_to_player()
-        blackjack()
-        #print(f'{hand_value(players_hand)} - {players_hand}')
-    else:
+    if player_cards_dealt < 2:
         players_hand.append(random.choice(current_game_deck))
         for card in players_hand:
             if card in current_game_deck:
                 current_game_deck.remove(card)
         player_cards_dealt += 1
-        
+        initial_deal()
+    else:
+        blackjack()
+        deal_cards_to_player()
 
-def blackjack(): # CONVERT THIS INTO A BLACKJACK DETECTOR
-    global dealers_hand, running, game_state
+
+def blackjack() -> str: # CONVERT THIS INTO A BLACKJACK DETECTOR
+    global dealers_hand, running, game_state, previous_round_message
+    previous_round_state = 'First Round'
         # Deals a card to player and removes it from the deck.
     if hand_value(dealers_hand) == 21 and hand_value(players_hand) == 21:
-        print (f'TIE')
+        previous_round_message = ('TIE')
+        previous_round_state = 'Tie'
         clear_hands()
     elif hand_value(dealers_hand) == 21:
-        print (f'Dealer Wins!')
+        previous_round_message = (f'Dealer Wins! {dealers_hand}')
+        previous_round_state = 'dealer win'
         clear_hands()
     elif hand_value(players_hand) == 21:
-        print(f'Blackjack! Player wins!')
-        game_state = 'blackjack'
-
+        previous_round_message = (f'Blackjack! Player wins!')
+        previous_round_state = 'blackjack'
     elif hand_value(players_hand) > 21:
-        print(f'Busted with {hand_value(players_hand)-21} Over!\n{players_hand}')
-        game_state = 'busted'
-    else:
-        hand_value(players_hand)
-        
+        previous_round_message = (f'Busted with {hand_value(players_hand)-21} Over! \n{players_hand}')
+        previous_round_state = 'busted'
+        clear_hands()
+        # print(f'Your hand has {hand_value(players_hand)} points: {players_hand}')
+    return previous_round_state
 
 def deal_cards_to_player():
-    global current_game_deck, players_hand, running, player_cards_dealt, stand_or_hit    
-    if blackjack() != 'Blackjack!':
-        print(f'Your hand has {hand_value(players_hand)} points: {players_hand}')
+    global current_game_deck, players_hand, running, player_cards_dealt, stand_or_hit
+    # os.system('cls')
+    print(f'{game_state} - {previous_round_message}\nThe Dealer\'s first card is always hidden!\n-------------------------------\n')
+    if blackjack() != 'blackjack' and blackjack() != 'busted':
+        print(f'Dealer\'s Hand: {hand_value(dealers_hand[1:])} - {dealers_hand[1:]}\nPlayer\'s Hand: {hand_value(players_hand)} - {players_hand}\n')
+        #print(f'Your hand has {hand_value(players_hand)} points: {players_hand}')
         stand_or_hit = input('Hit or Stand? ')
         if stand_or_hit.lower() == 'hit':
             players_hand.append(random.choice(current_game_deck))
@@ -103,7 +111,7 @@ def deal_cards_to_player():
                     current_game_deck.remove(card)
         elif stand_or_hit.lower() == 'stand':
             'PASSED STAND'
-            deal_cards_to_dealer()
+        deal_cards_to_dealer()
 
 def hand_value(cards_total):
     sum = 0
@@ -132,7 +140,6 @@ def hand_value(cards_total):
 
 def deal_cards_to_dealer():
     global dealers_hand, dealer_cards_dealt
-    print(f'Dealers Hand, First Hidden: {dealers_hand[1:]}')
     print(f'Dealer test passed')
 
 run_game()
