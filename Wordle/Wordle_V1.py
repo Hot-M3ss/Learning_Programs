858bf5d1c7d8391cb60754d2403cc11f86c622a9
Andrew <71112588+Hot-M3ss@users.noreply.github.com>
2024-09-27 22:45:49 -0400

    13th commit: Wordle_V1 is complete

--- Wordle/Wordle_V1.py
+++ Wordle/Wordle_V1.py
@@ -3,9 +3,6 @@ import string
 import os
 
 
-def menu():
-	pass
-
 def create_word_key(word) -> dict:
 	letter_key: dict = {}
 	for letter in string.ascii_lowercase:
@@ -54,66 +51,88 @@ def correctly_placed(word, guess) -> list[str]:
 	return current_guess
 
 
+def get_word() -> str:
+	os.system('cls')
+	while (True):
+		try:
+			word_length: int = input('How many letters do you want your word to have? ')
+			word: str = requests.get(f'https://random-word-api.herokuapp.com/word?length={word_length}').json()[0]
+			return word
+		except ValueError:
+			print('Must be a number!', end = ' ')
+		except IndexError:
+			print('The word length cannot be less than 2 or greater than 15!')
+		except ConnectionError | TimeoutError:
+			print('Unable to connect to https://random-word-api.herokuapp.com/word')
+			exit()
+
+
+def max_guesses() -> int:
+	while (True):
+		max_attempts = 5
+		try:
+			max_attempts: int = int(input('How many guesses would you like? (Default: 5) '))
+			if max_attempts < 1: raise ValueError
+			return max_attempts
+		except ValueError:
+			print('Must be a number greater than 0!', end = ' ')
+
+
+all_guesses: list[str] = []
+new_word: bool = True
+word: str = get_word()
+max_attempts = max_guesses()
+
+
 def continue_game() -> None:
-	global new_word, word, guess_attempt, all_guesses
+	global new_word, word, all_guesses, max_attempts
 	continue_input: str = input('\nWould you like to continue? ').lower()
 	match continue_input:
 		case 'yes':
 			new_word = True
 			all_guesses = []
-			guess_attempt = 0
 			word = get_word()
-		case _:
+			max_attempts = max_guesses()
+			os.system('cls')
+		case 'no':
 			exit()
+		case _:
+			print('Please enter yes or no.')
 
 
-def get_word() -> str:
-	os.system('cls')
-	while (True):
-		word_length: int = input('How many letters do you want your word to have? ')
-		if word_length in string.digits and word_length != '':
-			break
-		else:
-			print('Must be a number!', end = ' ')
-	word: str = requests.get(f'https://random-word-api.herokuapp.com/word?length={word_length}').json()[0]
-	return word
-
-all_guesses: list[str] = []
-new_word: bool = True
-word: str = get_word()	
-
-def play() -> None:
-	global new_word, word
+def main() -> None:
+	global new_word, word, max_attempts
 	guess_attempt: int = 0
 	
 	while (True):
+		# Checks to see if the player has reached the max attempt.
+		if guess_attempt == max_attempts:
+			print(f'\nYou Lose! The word was: {word}')
+			guess_attempt: int = 0
+			continue_game()
+		# Shows the max attempts and word length.
 		os.system('cls')
-		print(f'Welcome, you get 5 tries!\n')
+		print(f'Welcome, you have {max_attempts-guess_attempt} tries!\n')
 		if new_word == True:
 			for i in word:
 				print('[]', end=' ')
 			print()
 		else:
 			print_all_guesses(current_guess, guess, word)
-
+		# Prompts the user for a word/guess.
 		guess = input('\nCorrect: + | Incorrect: - | Wrong Spot: ~'
 				'\nPlease guess a word: ').lower()
-
+		# Checks to make sure the guess and word are the same length.
 		if len(guess) == len(word):
 			new_word = False
+			guess_attempt += 1
 			if guess == word:
-				os.system('cls')
 				print('\nYou Win!')
-				continue_game()
-			elif guess_attempt > 4:
-				os.system('cls')
-				print(f'\nYou Lose! The word was: {word}')
+				guess_attempt: int = 0
 				continue_game()
 			else:
-				current_guess = correctly_placed(word, guess) # was compare_guess()
-			guess_attempt += 1
-
+				current_guess = correctly_placed(word, guess)
 
 
 if __name__ == '__main__':
-	play()
+	main()
