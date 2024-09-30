8d266981d89031d1c8e53044d58645dd7c806ce3
Andrew <71112588+Hot-M3ss@users.noreply.github.com>
2024-09-29 23:07:52 -0400

    16th commit: working on BlackJack_V2

--- Blackjack/BlackJack_V2.py
+++ Blackjack/BlackJack_V2.py
@@ -1,5 +1,8 @@
+import random
+
 class Player:
     def __init__(self, name) -> None:
+        '''Variables: name, hand, chips, dealer'''
         self.name = name
         self.hand = []
         self.chips = 500
@@ -11,28 +14,61 @@ class Player:
     def __repr__(self) -> str:
         return self.name
 
-tim = Player('Tim')
+players: dict = {}
 
-
-def create_players():
+def create_players():     # for p in players: print(players[p].name)
+    global players
     while (True):
         try:
             player_num_input = int(input('How many players? '))
-            if player_num_input < 8:
-                players: dict = dict([(f'player_{i+1}', i) for i in range(player_num_input)])
-                print(players)
-                break
-            else:
-                print('Players are limited to 7 total.')
+            if 7 < player_num_input <= 0: raise ValueError
+            players: dict = dict([(f'player_{i+1}', i) for i in range(player_num_input)])
+            print(players)
+            break
         except ValueError:
-            print('Must be a number!', end=' ')
+            print('Must be a number! Must be less than 8!', end=' ')
         except TypeError:
-            print('Must be a number!', end=' ')
+            print('Must be a number! Must be less than 8!', end=' ')
 
     for entries in players:
         players[entries] = Player(input(f'Player Name: '))
 
-    # for p in players:
-    #     print(players[p].name)
+def set_dealer():
+    # add logic that rotates the dealer after a full round. [Low Priority]
+    ...
 
-create_players()
+
+def create_deck_input(default: bool) -> None:
+    if default is True:
+        # add logic that determines the number of decks, possibly using match, and then based on the number of players pass the number of decks to create_deck().
+        ...
+    else:
+        # add logic that prompts the player for an input, if that input is within the range 1:8, pass it to create_deck():
+        ...
+
+
+def create_deck(num_of_decks: int) -> list:
+    ten_value_cards = ['J','Q','K']
+    suits = ['H','C','D','S']
+    deck = []
+
+    while (True):
+        if num_of_decks == 0: break
+        try:
+            if not(8 >= int(num_of_decks) > 0): raise ValueError
+            for suit in suits:
+                deck.append(f'{suit}-{'A'}')
+                [deck.append(f'{suit}-{str(i+2)}') for i in range(9)]
+                [deck.append(f'{suit}-{card}') for card in ten_value_cards]
+            num_of_decks -= 1
+        except ValueError:
+            print('It must be a number, or if it already is must be less than 8.\nSetting default at 2')
+            num_of_decks: int = 2
+            
+    random.shuffle(deck)
+    return deck
+    
+
+
+
+create_deck(-1)
--- Wordle/Wordle_V1.py
+++ Wordle/Wordle_V1.py
@@ -3,68 +3,68 @@ import os
 
 
 def create_word_key(word) -> dict:
-	letter_key = dict([(letter, word.count(letter)) for letter in word]) 
-	return letter_key
+    letter_key = dict([(letter, word.count(letter)) for letter in word]) 
+    return letter_key
 
 
 def print_all_guesses(current_guess, guess, word) -> None:
-	if len(guess) == len(word):
-		all_guesses.append(f'{current_guess} - {guess}')
-		for value in all_guesses:
-			print(value)
-	else:
-		os.system('cls')
-		print('Incorrect Length\n')
-		for value in all_guesses:
-			print(value)
+    if len(guess) == len(word):
+        all_guesses.append(f'{current_guess} - {guess}')
+        for value in all_guesses:
+            print(value)
+    else:
+        os.system('cls')
+        print('Incorrect Length\n')
+        for value in all_guesses:
+            print(value)
 
 
 def correctly_placed(word, guess) -> list[str]:
-	word_key: dict = create_word_key(word)
-	letter_number: int = 0
-	init_guess: list[str] = ['-' for letter in word]
+    word_key: dict = create_word_key(word)
+    letter_number: int = 0
+    init_guess: list[str] = ['-' for letter in word]
 
-	for letter in guess:
-		# The below is used to initialize a letter
-		if letter == word[letter_number] and word_key.get(letter, 0) > 0:
-			init_guess[letter_number] = '+'
-			word_key[letter] -= 1
-		letter_number += 1
+    for letter in guess:
+        # The below is used to initialize a letter
+        if letter == word[letter_number] and word_key.get(letter, 0) > 0:
+            init_guess[letter_number] = '+'
+            word_key[letter] -= 1
+        letter_number += 1
 
-	letter_number: int = 0
-	for letter in guess:
-		if letter in word and word_key.get(letter, 0) > 0 and init_guess[letter_number] != '+':
-			init_guess[letter_number] = '~'
-			word_key[letter] -= 1
-		letter_number += 1
-	return init_guess
+    letter_number: int = 0
+    for letter in guess:
+        if letter in word and word_key.get(letter, 0) > 0 and init_guess[letter_number] != '+':
+            init_guess[letter_number] = '~'
+            word_key[letter] -= 1
+        letter_number += 1
+    return init_guess
 
 
 def get_word() -> str:
-	os.system('cls')
-	while (True):
-		try:
-			word_length: int = input('How many letters do you want your word to have? ')
-			word: str = requests.get(f'https://random-word-api.herokuapp.com/word?length={word_length}').json()[0]
-			return word
-		except ValueError:
-			print('Must be a number!', end = ' ')
-		except IndexError:
-			print('The word length cannot be less than 2 or greater than 15!')
-		except ConnectionError | TimeoutError:
-			print('Unable to connect to https://random-word-api.herokuapp.com/word')
-			exit()
+    os.system('cls')
+    while (True):
+        try:
+            word_length: int = input('How many letters do you want your word to have? ')
+            word: str = requests.get(f'https://random-word-api.herokuapp.com/word?length={word_length}').json()[0]
+            return word
+        except ValueError:
+            print('Must be a number!', end = ' ')
+        except IndexError:
+            print('The word length cannot be less than 2 or greater than 15!')
+        except ConnectionError | TimeoutError:
+            print('Unable to connect to https://random-word-api.herokuapp.com/word')
+            exit()
 
 
 def max_guesses() -> int:
-	while (True):
-		max_attempts = 5
-		try:
-			max_attempts: int = int(input('How many guesses would you like? (Default: 5) '))
-			if max_attempts < 1: raise ValueError
-			return max_attempts
-		except ValueError:
-			print('Must be a number greater than 0!', end = ' ')
+    while (True):
+        max_attempts = 5
+        try:
+            max_attempts: int = int(input('How many guesses would you like? (Default: 5) '))
+            if max_attempts < 1: raise ValueError
+            return max_attempts
+        except ValueError:
+            print('Must be a number greater than 0!', end = ' ')
 
 
 all_guesses: list[str] = []
@@ -74,54 +74,54 @@ max_attempts = max_guesses()
 
 
 def continue_game() -> None:
-	global new_word, word, all_guesses, max_attempts
-	continue_input: str = input('\nWould you like to continue? ').lower()
-	match continue_input:
-		case 'yes':
-			new_word = True
-			all_guesses = []
-			word = get_word()
-			max_attempts = max_guesses()
-			os.system('cls')
-		case 'no':
-			exit()
-		case _:
-			print('Please enter yes or no.')
+    global new_word, word, all_guesses, max_attempts
+    continue_input: str = input('\nWould you like to continue? ').lower()
+    match continue_input:
+        case 'yes':
+            new_word = True
+            all_guesses = []
+            word = get_word()
+            max_attempts = max_guesses()
+            os.system('cls')
+        case 'no':
+            exit()
+        case _:
+            print('Please enter yes or no.')
 
 
 def main() -> None:
-	global new_word, word, max_attempts
-	guess_attempt: int = 0
-	
-	while (True):
-		# Checks to see if the player has reached the max attempt.
-		if guess_attempt == max_attempts:
-			print(f'\nYou Lose! The word was: {word}')
-			guess_attempt: int = 0
-			continue_game()
-		# Shows the max attempts and word length.
-		os.system('cls')
-		print(f'Welcome, you have {max_attempts-guess_attempt} tries!\n')
-		if new_word == True:
-			for i in word:
-				print('[]', end=' ')
-			print()
-		else:
-			print_all_guesses(current_guess, guess, word)
-		# Prompts the user for a word/guess.
-		guess = input('\nCorrect: + | Incorrect: - | Wrong Spot: ~'
-				'\nPlease guess a word: ').lower()
-		# Checks to make sure the guess and word are the same length.
-		if len(guess) == len(word):
-			new_word = False
-			guess_attempt += 1
-			if guess == word:
-				print('\nYou Win!')
-				guess_attempt: int = 0
-				continue_game()
-			else:
-				current_guess = correctly_placed(word, guess)
+    global new_word, word, max_attempts
+    guess_attempt: int = 0
+
+    while (True):
+    # Checks to see if the player has reached the max attempt.
+        if guess_attempt == max_attempts:
+            print(f'\nYou Lose! The word was: {word}')
+            guess_attempt: int = 0
+            continue_game()
+        # Shows the max attempts and word length.
+        os.system('cls')
+        print(f'Welcome, you have {max_attempts-guess_attempt} tries!\n')
+        if new_word == True:
+            for i in word:
+                print('[]', end=' ')
+                print()
+        else:
+            print_all_guesses(current_guess, guess, word)
+        # Prompts the user for a word/guess.
+        guess = input('\nCorrect: + | Incorrect: - | Wrong Spot: ~'
+            '\nPlease guess a word: ').lower()
+        # Checks to make sure the guess and word are the same length.
+        if len(guess) == len(word):
+            new_word = False
+            guess_attempt += 1
+            if guess == word:
+                print('\nYou Win!')
+                guess_attempt: int = 0
+                continue_game()
+            else:
+                current_guess = correctly_placed(word, guess)
 
 
 if __name__ == '__main__':
-	main()
+    main()
