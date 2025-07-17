98bd76e6719c3732f11f16ce5fb17e35928d836d
Raven <avn_raven@icloud.com>
2025-07-17 00:08:08 -0400

    32nd commit: refactored todolist.py

--- Python/small_projects/test.py
+++ Python/small_projects/test.py
@@ -1,53 +1,62 @@
-def testOne():
-    while(True):
-        test = input("Number: ").replace(",","")
-        number = verifyNumber(test)
-        print(f"{type(number)} success: {number}")
-        
-def verifyNumber(number):
-    try:
-        return int(number)
-    except ValueError:
-        print(f"Int Fail: {number}")
+# def testOne():
+#     while(True):
+#         test = input("Number: ").replace(",","")
+#         number = verifyNumber(test)
+#         print(f"{type(number)} success: {number}")
 
-    try:
-        return float(number)
-    except:
-        print(f"Float Fail: {number}")
+# def verifyNumber(number):
+#     try:
+#         return int(number)
+#     except ValueError:
+#         print(f"Int Fail: {number}")
 
-testOne()
+#     try:
+#         return float(number)
+#     except:
+#         print(f"Float Fail: {number}")
 
-def testTwo():
-    while(True):
-        raw = input("Number: ")
-        cleaned = raw.replace(",","")
-        number = verifyNumber(cleaned)
-        print(f"{type(number)} success: {number}")
-        
-def verifyNumber(number):
-    try:
-        return int(number)
-    except ValueError:
-        pass
-    
-    try:
-        return float(number)
-    except ValueError:
-        return None
+# testOne()
 
-testTwo()
+# def testTwo():
+#     while(True):
+#         raw = input("Number: ")
+#         cleaned = raw.replace(",","")
+#         number = verifyNumber(cleaned)
+#         print(f"{type(number)} success: {number}")
+
+# def verifyNumber(number):
+#     try:
+#         return int(number)
+#     except ValueError:
+#         pass
+
+#     try:
+#         return float(number)
+#     except ValueError:
+#         return None
+
+# testTwo()
+
+# def viewList(loadedList, includeUpdateList: bool):
+#     print("Welcome to your todo list!\n")
+#     if loadedList != []:
+#         maxValueLength = max(len(x['task']) for x in loadedList)
+#         for id, item in enumerate(loadedList, start=1):
+#             status = f'Due by {item["date"]:<10}' if item["done"] == False else "Completed"
+#             print(f'{id:>3}. Task: {item["task"]:<{maxValueLength}} | Status: {status:<17} | Note: {item["note"]:<10}')
+#     else:
+#         print(f"No items loaded; has a file been generated?")
+#     if includeUpdateList == True:
+#         maxKeyLength = max(len(x) for x in loadedList[taskNumber])
+#         for id, item in enumerate(loadedList[taskNumber], start=1):
+#             print(f'{id:>3}. {f'{item}:':<{maxKeyLength+1}} {loadedList[taskNumber][item]}')
 
 
+# maxTaskLength = max(len(x) for x in newList[taskNumber])
+# for id, item in enumerate(newList[taskNumber], start=1):
+#     print(f'{id:>3}. {f'{item}:':<{maxTaskLength+1}} {newList[taskNumber][item]}')
 
-
-
-maxTaskLength = max(len(x) for x in newList[taskNumber])
-for id, item in enumerate(newList[taskNumber], start=1):
-    print(f'{id:>3}. {f'{item}:':<{maxTaskLength+1}} {newList[taskNumber][item]}')
-
-maxTaskLength = max(len(x['task']) for x in loadedList)
-for id, item in enumerate(loadedList, start=1):
-    status = f'Due by {item["date"]:<10}' if item["done"] == False else "Completed"
-    print(f'{id:>3}. Task: {item["task"]:<{maxTaskLength}} | Status: {status:<17} | Note: {item["note"]:<10}')
-
-
+# maxTaskLength = max(len(x['task']) for x in loadedList)
+# for id, item in enumerate(loadedList, start=1):
+#     status = f'Due by {item["date"]:<10}' if item["done"] == False else "Completed"
+#     print(f'{id:>3}. Task: {item["task"]:<{maxTaskLength}} | Status: {status:<17} | Note: {item["note"]:<10}')
--- Python/small_projects/todo_list.py
+++ Python/small_projects/todo_list.py
@@ -1,20 +1,22 @@
 import os
 import json
 
-def main():
+
+def main() -> None:
     desktop = os.path.join(os.path.expanduser("~"), "Documents")
     filePath = os.path.join(desktop, "todoList.json")
     loadedList = loadExistingTodoList(filePath)
-    
     optionSelect(filePath, loadedList)
 
-def clearTerminal():
+
+def clearTerminal() -> None:
     if os.name == "posix":
         os.system("clear")
     elif os.name == "nt":
         os.system("cls")
 
-def loadExistingTodoList(filePath):
+
+def loadExistingTodoList(filePath) -> list:
     try:
         with open(filePath, 'r') as file:
             loadedList = json.load(file)
@@ -23,18 +25,20 @@ def loadExistingTodoList(filePath):
         todoList = []
         return todoList
 
-def viewList(loadedList):
+
+def viewList(loadedList) -> None:
     print("Welcome to your todo list!\n")
     if loadedList != []:
         maxTaskLength = max(len(x['task']) for x in loadedList)
-        for id, item in enumerate(loadedList, start=1):
-            status = f'Due by {item["date"]:<10}' if item["done"] == False else "Completed"
-            print(f'{id:>3}. Task: {item["task"]:<{maxTaskLength}} | Status: {status:<17} | Note: {item["note"]:<10}')
+        for idx, item in enumerate(loadedList, start=1):
+            status = f'Due by {item["date"]:<10}' if item["done"] is False else "Completed"
+            print(f'{idx:>3}. Task: {item["task"]:<{maxTaskLength}} | Status: {status:<17} | Note: {item["note"]:<10}')
     else:
-        print(f"No items loaded; has a file been generated?")
+        print("No items loaded; has a file been generated?")
 
-def optionSelect(filePath, loadedList):
-    while(True):
+
+def optionSelect(filePath, loadedList) -> None:
+    while True:
         clearTerminal()
         viewList(loadedList)
 
@@ -51,47 +55,50 @@ def optionSelect(filePath, loadedList):
                 break
             case _:
                 print("Not a valid option!")
-    
-def addTask(loadedList):
+
+
+def addTask(loadedList) -> list:
     clearTerminal()
     viewList(loadedList)
-    while (True):
+    while True:
         match input("\nWould you like to add a task? (y/n): ").lower():
             case 'y':
                 todoname = input("\nTask Name: ")
-                todoDate  = input("Task Due (yyyy-mm-dd): ")
+                todoDate = input("Task Due (yyyy-mm-dd): ")
                 todoNote = input("Task Note: ")
                 loadedList.append({"task": todoname, "date": todoDate, "note": todoNote, "done": False})
                 return loadedList
             case _:
                 return loadedList
 
-def updateTask(loadedList):
+
+def updateTask(loadedList) -> list:
     newList = copyList(loadedList)
     clearTerminal()
     viewList(newList)
     taskNumber = int(input("\nWhich task would you like to open?: "))-1
-    while(True):
+    while True:
         clearTerminal()
         viewList(newList)
-        print("\nPlease choose an option below. \n")
-        maxTaskLength = max(len(x) for x in newList[taskNumber])
-        for id, item in enumerate(newList[taskNumber], start=1):
-            print(f'{id:>3}. {f'{item}:':<{maxTaskLength+1}} {newList[taskNumber][item]}')
-        
-        print(f"\n{'D':>3}. discard changes")
-        print(f"{'S':>3}. save changes")
+        print("\nPlease choose an option below.\n")
+        maxTaskLength = max(len(x) + 1 for x in newList[taskNumber])
+        for idx, item in enumerate(newList[taskNumber], start=1):
+            print(f'{idx:>3}. {item + ":":<{maxTaskLength}} {newList[taskNumber][item]}')
+        print(f"\n{'d':>3}. discard changes \n{'s':>3}. save changes")
 
         match input("\nOption: ").lower():
             case '1':
-                newList[taskNumber].update({'task':input("\nNew Task Name: ")})
+                newList[taskNumber].update(
+                    {'task': input("\nNew Task Name: ")})
             case '2':
-                newList[taskNumber].update({'date':input("\nNew due date (yyyy-mm-dd): ")})
+                newList[taskNumber].update(
+                    {'date': input("\nNew due date (yyyy-mm-dd): ")})
             case '3':
-                newList[taskNumber].update({'note':input("\nNew Task Note: ")})
+                newList[taskNumber].update(
+                    {'note': input("\nNew Task Note: ")})
             case '4':
                 oppositeValue = (False if newList[taskNumber]['done'] == True else True)
-                newList[taskNumber].update({'done':oppositeValue})
+                newList[taskNumber].update({'done': oppositeValue})
             case 's':
                 return newList
             case 'd':
@@ -99,12 +106,13 @@ def updateTask(loadedList):
             case _:
                 print("Not a valid option")
 
-def deleteTask(loadedList):
+
+def deleteTask(loadedList) -> list:
     newList = copyList(loadedList)
-    while(True):
+    while True:
         clearTerminal()
         viewList(newList)
-        print('\n1. delete a task\n\nD. discard changes\nS. save changes')
+        print('\n1. delete a task\n\nd. discard changes\ns. save changes')
         match input("\nWould would you like to do?: ").lower():
             case '1':
                 try:
@@ -116,25 +124,29 @@ def deleteTask(loadedList):
                     if len(loadedList) == 0:
                         print("There are no entries to delete!")
                     else:
-                        print(f"You can delete a range of 1-{len(newList)} entries.")
+                        print(
+                            f"You can delete a range of 1-{len(newList)} entries.")
             case 'd':
                 return loadedList
             case 's':
                 return newList
 
-def copyList(loadedList):
+
+def copyList(loadedList) -> list:
     newList = []
     for x in loadedList:
         newDict = {}
         for item in x:
             keyValue = x[item]
-            newDict.update({item:keyValue})
-        newList.append(newDict) 
+            newDict.update({item: keyValue})
+        newList.append(newDict)
     return newList
 
-def writeToFile(filePath, itemToWrite):
+
+def writeToFile(filePath, itemToWrite) -> None:
     with open(filePath, 'w') as file:
         json.dump(itemToWrite, file, indent=4)
     print("write successful!")
+
 
 main()
